f60ec7eff5bb5589c68ee32236db9a610a534f5c
Daniel Flores Rodríguez <danoman@Danomans-MacBook-Air-2.local>
2022-09-09 02:36:13 -0500

    added first display

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,55 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import TextBox from './lib/TextBox.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="container-general">
+    <div class="container-above">
+      <div class="card-general-container">
+
+        <div class="title">
+          <h1> American Express Customer Default Prediction </h1>
+        </div>
+
+        <div class="input-section">
+          <div class="subtitle-input-section">
+            <p> Select a customer: </p>
+          </div>
+          <div class="input-component-container">
+              <TextBox/>
+            <button id="btn-random" >
+              <i class="material-icons" style="font-size: 35px;" id="btn-dice">casino</i>
+            </button>
+          </div>
+        </div>
+        <div class="buton-container">
+          <button id="btn-predict">
+              <div class="containe-btn-predict">
+
+              Predict
+              <i class="material-icons" style="font-size: 45px;"> pages </i>
+            </div>
+
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="container-footer">
+      <p>
+        Model: Neural network with 3 hiden layers from Scikit-learn MLP Model using ReLu as activaction function and Adam algorithm as solver.
+      </p>
+      <p>
+        Dataset from: 
+        <a href="https://www.kaggle.com/competitions/amex-default-prediction/overview"
+          id="link-to-kaggle">
+          https://www.kaggle.com/competitions/amex-default-prediction/overview
+        </a> 
+      </p>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
---
+++ src/lib/TextBox.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    function getValue( event ){
+        console.log( event.target.value );
+    }
+</script>
+
+
+<input id="txtInput" type="text" on:input={ (e) => getValue(e) }>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,600;1,700&display=swap');
+
+#txtInput{
+    height: 50px;
+    width: 89%;
+    padding: 3px;
+    font-family: 'Montserrat', sans-serif;
+    color: #595353;
+    font-weight: 600;
+    font-size: 15px;
+    border-radius: 5px;
+    border-style: none;
+    margin-top: 10px;
+}
+
+
+</style>
